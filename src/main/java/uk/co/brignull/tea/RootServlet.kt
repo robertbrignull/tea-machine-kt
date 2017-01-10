0f2f721138d85b6f621bee4fc561b6ef81517d11
Robert Brignull <robertbrignull@gmail.com>
2017-01-10 23:38:33 +0000

    add "Add to Slack" button to root page

--- src/main/java/uk/co/brignull/tea/RootServlet.kt
+++ src/main/java/uk/co/brignull/tea/RootServlet.kt
@@ -1,5 +1,6 @@
 package uk.co.brignull.tea
 
+import com.mitchellbosecke.pebble.PebbleEngine
 import java.io.IOException
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServlet
@@ -9,5 +10,12 @@ import javax.servlet.http.HttpServletResponse
 class RootServlet : HttpServlet() {
     @Throws(ServletException::class, IOException::class)
     public override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val engine = PebbleEngine.Builder().build()
+        val compiledTemplate = engine.getTemplate("templates/root.html")
+
+        val context = mutableMapOf<String, Any>()
+        context.put("client_id", "64369940551.124605742784")
+
+        compiledTemplate.evaluate(resp.writer, context)
     }
 }

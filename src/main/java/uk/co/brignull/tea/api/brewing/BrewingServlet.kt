3a070791dc63704d54c7f96e4f598986ee5e8806
Robert Brignull <robertbrignull@gmail.com>
2017-01-10 23:19:06 +0000

    rename servlet

--- src/main/java/uk/co/brignull/tea/api/brewing/BrewingServlet.kt
+++ src/main/java/uk/co/brignull/tea/api/brewing/BrewingServlet.kt
@@ -1,4 +1,4 @@
-package uk.co.brignull.tea
+package uk.co.brignull.tea.api.brewing
 
 import java.io.IOException
 import javax.servlet.ServletException
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class TeaMachine : HttpServlet() {
+class BrewingServlet : HttpServlet() {
     @Throws(ServletException::class, IOException::class)
     public override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
         resp.writer.println("Hello, World!")

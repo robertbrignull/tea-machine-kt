0a3f138e11a6bf13f04ffa8195e8158733c5d35e
Robert Brignull <robertbrignull@gmail.com>
2017-01-14 22:13:01 +0000

    change model classes to data classes

--- src/main/java/uk/co/brignull/tea/model/objects/AuthUser.kt
+++ src/main/java/uk/co/brignull/tea/model/objects/AuthUser.kt
@@ -4,18 +4,9 @@ import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 
 @Entity
-class AuthUser {
-    @Id var teamName: String?
-    var teamID: String?
-    var accessToken: String?
-    var scope: String?
-
-    constructor() : this(null, null, null, null)
-
-    constructor(teamName: String?, teamID: String?, accessToken: String?, scope: String?) {
-        this.teamName = teamName
-        this.teamID = teamID
-        this.accessToken = accessToken
-        this.scope = scope
-    }
-}
+data class AuthUser(
+    @Id var teamName: String? = null,
+    var teamID: String? = null,
+    var accessToken: String? = null,
+    var scope: String? = null
+)
--- src/main/java/uk/co/brignull/tea/model/objects/OAuthConfiguration.kt
+++ src/main/java/uk/co/brignull/tea/model/objects/OAuthConfiguration.kt
@@ -4,18 +4,9 @@ import com.googlecode.objectify.annotation.Entity
 import com.googlecode.objectify.annotation.Id
 
 @Entity
-class OAuthConfiguration {
-    @Id var id: String?
-    var clientId: String?
-    var clientSecret: String?
-    var verificationToken: String?
-
-    constructor() : this(null, null, null, null)
-
-    constructor(id: String?, clientId: String?, clientSecret: String?, verificationToken: String?) {
-        this.id = id
-        this.clientId = clientId
-        this.clientSecret = clientSecret
-        this.verificationToken = verificationToken
-    }
-}
+data class OAuthConfiguration(
+    @Id var id: String? = null,
+    var clientId: String? = null,
+    var clientSecret: String? = null,
+    var verificationToken: String? = null
+)
